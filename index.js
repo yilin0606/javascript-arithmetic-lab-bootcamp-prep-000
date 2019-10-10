f8b8ebb9986eb3f75d98d27dcdfebb740e762d62
Yilin Freya Gao <thisisgyl@gmail.com>
2019-10-10 21:07:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,17 +33,8 @@ function makeInt(n){
 
 
 function preserveDecimal(n){
-  if 
+  return parseFloat(n)
 }
 
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
 
 
