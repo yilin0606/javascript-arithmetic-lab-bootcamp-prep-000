c4a8667a95a16ee55df95985b27049a3228d0e10
Yilin Freya Gao <thisisgyl@gmail.com>
2019-10-10 20:51:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,29 +26,10 @@ function dec(a){
 
 
 function makeInt(n){
-  if (a.toString=a){
-    return a
+  if (n.toString()===n){
+    return parseInt(n,10)
   }
-  else if ("n"=0x2328, a=10){
-    return "0"
-  }
-  else if ("n" isNaN(parsed)){
-    return "NaN"
-  }
-}
 
-describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
 
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
 
 
