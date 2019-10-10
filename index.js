d58973ddb6c2386698e57534e6fd84256b24219b
Yilin Freya Gao <thisisgyl@gmail.com>
2019-10-10 20:44:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a,b){
+return a+b
+}
+
+function subtract(a,b){
+  return a-b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+var number=a
+function inc(a){
+  return a=a+1
+}
+
+var number=a
+function dec(a){
+  return a=a-1
+}
+
+
+function makeInt("n",a){
+  if (a.toString=a){
+    return a
+  }
+  else if ("n"=0x2328, a=10){
+    return "0"
+  }
+  else if ("n" isNaN(parsed)){
+    return "NaN"
+  }
+}
+
+
